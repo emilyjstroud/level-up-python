b99b683b6d7ce30baa1355746b6d129f2fffaae5
Emily Stroud <emilystroud93@gmail.com>
2022-12-02 21:59:50 -0600

    adding update functionality

--- levelupapi/views/event.py
+++ levelupapi/views/event.py
@@ -30,6 +30,22 @@ class EventView(ViewSet):
     serializer = EventSerializer(event)
     return Response(serializer.data)
     
+  def update(self, request, pk):
+    event = Event.objects.get(pk=pk)
+    event.description = request.data["description"]
+    event.date = request.data["date"]
+    event.time = request.data["time"]
+    
+    game = Game.objects.get(pk=request.data["game"])
+    game = game
+    event.save()
+    
+    return Response(None, status=status.HTTP_204_NO_CONTENT)
+  
+  def destroy(self, request, pk):
+    event = Event.objects.get(pk=pk)
+    event.delete()
+    return Response(None, status=status.HTTP_204_NO_CONTENT)
   
 class EventSerializer(serializers.ModelSerializer):
     class Meta:
